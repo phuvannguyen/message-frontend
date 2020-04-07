e0dc4a8ed0f21f9a10ba43b04a773d1decbe95c5
Phu Nguyen <phu21994@gmail.com>
2020-04-07 11:11:00 +0700

    Make a first basic app layout

---
+++ src/component/Main.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './Main.css'
+
+class Main extends Component {    
+
+    render() {       
+
+        return <div className="Main">
+            Main Stuff
+            
+        </div>
+
+
+    };
+    
+}
+
+export default Main
---
+++ src/component/Sidebar.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import './Sidebar.css'
+
+export default class Sidebar extends Component {
+    render() {
+        return <aside className="Sidebar">Sidebar</aside>
+    }
+}
