5d752c093a6cf959a119939baf9ac2f7a58d83d9
Phu Nguyen <phu21994@gmail.com>
2020-04-19 11:12:37 +0700

    Make a render when state was updated

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
+import store from "./Store/store"
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const render = () => {
+    fancyLog();
+    return ReactDOM.render(<App />, document.getElementById('root'))
+};
+render();
+store.subscribe(render);
+serviceWorker.register();
+function fancyLog() {
+    console.log("%c Rendered with", "background: purple; color: #FFF");
+    console.log(store.getState());
+}
+
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
