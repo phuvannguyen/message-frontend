a1e13877bf086873b20982a95bf6892b7406b41e
Phu Nguyen <phu21994@gmail.com>
2020-04-19 11:08:30 +0700

    Make a basic sidebar

--- src/component/Sidebar.js
+++ src/component/Sidebar.js
@@ -1,8 +1,14 @@
 import React, { Component } from "react";
+import User from "../containers/User"
 import './Sidebar.css'
 
+
 export default class Sidebar extends Component {
+
     render() {
-        return <aside className="Sidebar">Sidebar</aside>
+        const { contacts } = this.props;
+        return <aside className="Sidebar">
+            {contacts.map(contact => <User user={contact} key={contact.user_id}/>)}
+        </aside>
     }
 }
