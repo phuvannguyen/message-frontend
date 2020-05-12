3db0cdd2190bcde7bb741b06228a7879e1f6f374
Phu Nguyen <phu21994@gmail.com>
2020-05-12 09:43:40 +0700

    Refactor: Change Siderbar.js  from use normal class to combine react-redux which has mapStateToProps and mapDispatchToProps

--- src/component/Sidebar.js
+++ src/component/Sidebar.js
@@ -1,14 +1,34 @@
 import React, { Component } from "react";
-import User from "../containers/User"
-import './Sidebar.css'
+import User from "../containers/User";
+import './Sidebar.css';
+import {setActiveUserId} from "../actions/index"
+import { connect } from "react-redux";
+import _ from "lodash"
 
-
-export default class Sidebar extends Component {
+class Sidebars extends Component {
 
     render() {
-        const { contacts } = this.props;
+        const { contacts, handleUserClick} = this.props;                   
         return <aside className="Sidebar">
-            {contacts.map(contact => <User user={contact} key={contact.user_id}/>)}
+            {_.values(contacts).map(contact => <User user={contact} 
+                                                     key={contact.user_id} 
+                                                     handleUserClick={handleUserClick} />)}
         </aside>
     }
 }
+
+const  mapStateToProps = state => {
+    return {
+        contacts: state.contacts
+    }
+}
+const mapDispatchToProps = (dispatch) => {
+    return ({
+        handleUserClick: (user_id) => {
+            dispatch(setActiveUserId(user_id));
+        }
+    });
+}
+
+const Sidebar = connect(mapStateToProps, mapDispatchToProps)(Sidebars);
+export default Sidebar;
