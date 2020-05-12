e50fba11871d231eabe6e5ba84e1f667a6c5f188
Phu Nguyen <phu21994@gmail.com>
2020-05-12 09:41:11 +0700

    Refactor: Change Main.js from use normal class to combine react-redux which has mapStateToProps

--- src/component/Main.js
+++ src/component/Main.js
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
-import './Main.css'
+import './Main.css';
+import Empty from "./Empty";
+import ChatWindow from "./ChatWindow"
+import { connect} from "react-redux"
 
-class Main extends Component {    
+class Mains extends Component {    
 
-    render() {       
+    render() {
+    	const { userReducer, activeUserIdReducer} = this.props;
+    	const renderMainContent = () => {
+    		if (!activeUserIdReducer) {
+    			return <Empty user={userReducer} activeUserId={activeUserIdReducer} />
+    		} else return <ChatWindow user={userReducer} activeUserId={activeUserIdReducer} />
+    	}     
 
         return <div className="Main">
-            Main Stuff
+        	{renderMainContent()}            
             
         </div>
 
 
     };
     
+};
+const  mapStateToProps = state => {
+    return {
+		userReducer: state.userReducer,
+		activeUserIdReducer: state.activeUserIdReducer
+    }
 }
-
+const Main = connect(mapStateToProps)(Mains)
 export default Main
