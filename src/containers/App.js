0eddda03d1808dea3e256d9c8d461270104cc953
Phu Nguyen <phu21994@gmail.com>
2020-05-12 09:57:45 +0700

    Change: Change font-family

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,18 +2,15 @@ import React, {Component} from 'react';
 import './App.css';
 import Main from '../component/Main';
 import Sidebar from '../component/Sidebar';
-import store from '../Store/store';
-import  _ from "lodash";
 
 
 class App extends Component {
-  
 
-  render() {
-    const { contacts } = store.getState();
+  render() {     
+    
     return (
       <div className="App">        
-        <Sidebar contacts={_.values(contacts)}/>
+        <Sidebar  />
         <Main />
       </div>
     );
